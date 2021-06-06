3ab1deae474b47105d3abac67dad113c1865ed1b
harujiiiii <>
2021-06-06 23:01:17 +0000

    Create a function called create_line_geom () that takes a list of Shapely Point objects as a parameter called points and returns a LineString object for those input points. In addition, care must be taken to ensure that the function is used properly.

--- Exercise_1_problem_1_2.py
+++ Exercise_1_problem_1_2.py
@@ -17,11 +17,6 @@ point=Point(X_coord,y_coord)
 return point
 
 # Test your function by running these code cells:
-
-point1=create_point_geom(0.0,1.1)
-print(point1)
-print(point1.geom_type)
-
 # CODE FOR TESTING YOUR SOLUTION
 point1 = create_point_geom(0.0, 1.1)
 
@@ -35,12 +30,17 @@ print(point1.geom_type)
 # 
 
 # YOUR CODE HERE 2 to define create_line_geom()
-
+def create_line_geom(points):
+  assert type(points)==list,"Input should be a list!"
+  assert len(points)>=2,"LineString object requires at least two Points!"
+  line=LineString(points)
+  return line
 # Demonstrate the usage of your function; For example, create a line object with two points: `Point(45.2, 22.34)` & `Point(100.22, -3.20)` and store the result in a variable called `line1`:
 
 line1 = None
 # YOUR CODE HERE 3 to define two points and store the result in line1
-
+points=[(45.2,22.34),(100.22,-3.20)]
+line1=create_line_geom(points)
 
 # CODE FOR TESTING YOUR SOLUTION
 print(line1)
